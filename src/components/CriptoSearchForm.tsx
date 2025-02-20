6512b91c5a3fb96930f8fce3125c14cf3ba4a176
ValeLan <vale.lanf@gmail.com>
2025-02-19 22:41:43 -0300

    Manejo llamada a API de crypto utilizando axios y manejo de estado con zod y zustand

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,5 +1,23 @@
+import { useCryptoStore } from "../store"
+import { currencies } from "../data"
+import { useState } from "react"
+import { Pair } from "../types"
+import { ChangeEvent } from "react"
 
 const CriptoSearchForm = () => {
+    const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
+    const [pair, setPair] = useState<Pair>({
+        currency: '',
+        criptocurrency: ''
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>)=>{
+        setPair({
+            ...pair,
+            [e.target.name] : e.target.value
+        })
+    }
+
     return (
         <form
             action=""
@@ -10,8 +28,12 @@ const CriptoSearchForm = () => {
                 <select
                     name="currency"
                     id="currency"
+                    onChange={handleChange}
                 >
                     <option value="">--Seleccione--</option>
+                    {currencies.map(currency => 
+                        <option key={currency.code} value={currency.code}>{currency.name}</option>
+                    )}
                 </select>
             </div>
 
@@ -20,8 +42,15 @@ const CriptoSearchForm = () => {
                 <select
                     name="criptocurrency"
                     id="criptocurrency"
+                    onChange={handleChange}
                 >
                     <option value="">--Seleccione--</option>
+                    {cryptocurrencies.map(crypto => (
+                        <option 
+                            value={crypto.CoinInfo.FullName}
+                            key={crypto.CoinInfo.Name}
+                        >{crypto.CoinInfo.FullName}</option>
+                    ))}
                 </select>
             </div>
 
